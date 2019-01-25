90b856d63a3c4f50b89a65e86b0daa2befcb889d
Markus Felten <Markus.Felten@gmx.de>
2019-01-25 19:53:50 +0100

    chore(package): got@9.6.0 npm-pkgbuild@4.2.1

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,48 @@ import json from "rollup-plugin-json";
 import cleanup from "rollup-plugin-cleanup";
 import pkg from "./package.json";
 
+const external = [
+  "assert",
+  "async_hooks",
+  "buffer",
+  "child_process",
+  "cluster",
+  "console",
+  "constants",
+  "crypto",
+  "dgram",
+  "dns",
+  "domain",
+  "events",
+  "fs",
+  "http",
+  "http2",
+  "https",
+  "inspector",
+  "module",
+  "net",
+  "os",
+  "path",
+  "perf_hooks",
+  "process",
+  "punycode",
+  "querystring",
+  "readline",
+  "repl",
+  "stream",
+  "string_decoder",
+  "sys",
+  "timers",
+  "tls",
+  "trace_events",
+  "tty",
+  "url",
+  "util",
+  "v8",
+  "vm",
+  "zlib"
+];
+
 export default Object.keys(pkg.bin || {}).map(name => {
   return {
     input: `src/${name}-cli.mjs`,
@@ -26,15 +68,6 @@ export default Object.keys(pkg.bin || {}).map(name => {
       cleanup(),
       executable()
     ],
-    external: [
-      "fs",
-      "path",
-      "net",
-      "util",
-      "events",
-      "stream",
-      "child_process",
-      "assert"
-    ]
+    external
   };
 });
