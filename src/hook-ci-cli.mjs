079438dc9ffaa50875a6fbf7a1c6db1517fe361a
Markus Felten <Markus.Felten@gmx.de>
2019-04-09 23:06:18 +0200

    fix: use config

--- src/hook-ci-cli.mjs
+++ src/hook-ci-cli.mjs
@@ -29,112 +29,103 @@ program
           clone: {
             depth: 10
           }
-        }
+        },
         workspace: { dir: "${first(env.STATE_DIRECTORY,'/tmp')}" },
-        redis: { url: "${first(env.REDIS_URL,'redis://127.0.0.1:6379')}" }
+        redis: { url: "${first(env.REDIS_URL,'redis://127.0.0.1:6379')}" },
+        http: {
+          port: "${first(env.PORT,8093)}"
+        }
       }
     });
 
     console.log(config);
+
+    const requestQueue = new Queue("post-requests", config.redis.url);
+    const cleanupQueue = new Queue("cleanup", config.redis.url);
+    const errorQueue = new Queue("error", config.redis.url);
+
+    requestQueue.on("cleaned", (job, type) => {
+      console.log("requestQueue cleaned %s %s jobs", job.length, type);
+    });
+
+    cleanupQueue.process(async job => {
+      requestQueue.clean(5000);
+      //queue.clean(10000, 'failed');
+
+      console.log("cleanupQueue", job.data.after);
+
+      if (job.data.after) {
+        const wd = join(config.workspace.dir, job.data.after);
+
+        console.log(`rm -rf ${wd}`);
+
+        const proc = await execa("rm", ["-rf", wd]);
+      }
+    });
+
+    errorQueue.process(async job => {
+      console.log("errorQueue", job.data.error);
+    });
+
+    requestQueue.process(async job => {
+      try {
+        const result = await startJob(job);
+        cleanupQueue.add(job.data);
+        return result;
+      } catch (e) {
+        errorQueue.add({ error: e });
+        throw e;
+      }
+    });
+
+    requestQueue.on("completed", (job, result) => {
+      console.log("requestQueue completed", result);
+    });
+
+    const handler = createHookHandler(requestQueue);
+
+    const server = micro(async (req, res) => {
+      handler(req, res, err => {
+        if (err) {
+          console.log(err);
+          res.writeHead(404);
+          res.end("no such location");
+        } else {
+          res.writeHead(200);
+          res.end("woot");
+        }
+      });
+    });
+
+    server.listen(config.http.port);
+
+    async function startJob(job) {
+      const url = job.data.repository.url;
+      console.log("start: ", url);
+      const wd = join(config.workspace.dir, job.data.head_commit.id);
+
+      job.progress(1);
+
+      const proc = execa("git", ["clone", "--depth", "10", url, wd]);
+      proc.stdout.pipe(process.stdout);
+      proc.stderr.pipe(process.stderr);
+      await proc;
+
+      for (const pkg of await globby(["**/package.json"], { cwd: wd })) {
+        console.log("PACKAGE.JSON", pkg, dirname(pkg));
+        await runNpm(job, wd, dirname(pkg));
+      }
+
+      for (const pkg of await globby(["**/PKGBUILD"], { cwd: wd })) {
+        console.log("PKGBUILD", pkg, dirname(pkg));
+        await pkgbuild(job, wd, dirname(pkg));
+      }
+
+      return {
+        url,
+        wd,
+        arch: process.arch
+      };
+    }
   })
   .parse(process.argv);
-
-const dataDir = "/var/lib/hook-ci";
-const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
-
-const requestQueue = new Queue("post-requests", REDIS_URL);
-const cleanupQueue = new Queue("cleanup", REDIS_URL);
-const errorQueue = new Queue("error", REDIS_URL);
-
-requestQueue.on("cleaned", (job, type) => {
-  console.log("requestQueue cleaned %s %s jobs", job.length, type);
-});
-
-cleanupQueue.process(async job => {
-  requestQueue.clean(5000);
-  //queue.clean(10000, 'failed');
-
-  console.log("cleanupQueue", job.data.after);
-
-  if (job.data.after) {
-    const wd = join(dataDir, job.data.after);
-
-    console.log(`rm -rf ${wd}`);
-
-    const proc = await execa("rm", ["-rf", wd]);
-  }
-});
-
-errorQueue.process(async job => {
-  console.log("errorQueue", job.data.error);
-});
-
-requestQueue.process(async job => {
-  try {
-    const result = await startJob(job);
-    cleanupQueue.add(job.data);
-    return result;
-  } catch (e) {
-    errorQueue.add({ error: e });
-    throw e;
-  }
-});
-
-requestQueue.on("completed", (job, result) => {
-  console.log("requestQueue completed", result);
-});
-
-let port = "systemd";
-
-if (process.env.PORT !== undefined) {
-  port = parseInt(process.env.PORT, 10);
-  if (Number.isNaN(port)) {
-    port = process.env.PORT;
-  }
-}
-
-const handler = createHookHandler(requestQueue);
-
-const server = micro(async (req, res) => {
-  handler(req, res, err => {
-    if (err) {
-      console.log(err);
-      res.writeHead(404);
-      res.end("no such location");
-    } else {
-      res.writeHead(200);
-      res.end("woot");
-    }
-  });
-});
-
-server.listen(port);
-
-async function startJob(job) {
-  const url = job.data.repository.url;
-  console.log("start: ", url);
-  const wd = join(dataDir, job.data.head_commit.id);
-
-  job.progress(1);
-
-  const proc = execa("git", ["clone", "--depth", "10", url, wd]);
-  proc.stdout.pipe(process.stdout);
-  proc.stderr.pipe(process.stderr);
-  await proc;
-
-  for (const pkg of await globby(["**/package.json"], { cwd: wd })) {
-    console.log("PACKAGE.JSON", pkg, dirname(pkg));
-    await runNpm(job, wd, dirname(pkg));
-  }
-
-  for (const pkg of await globby(["**/PKGBUILD"], { cwd: wd })) {
-    console.log("PKGBUILD", pkg, dirname(pkg));
-    await pkgbuild(job, wd, dirname(pkg));
-  }
-
-  return {
-    url,
-    wd,
-    arch: process.arch
-  };
-}
