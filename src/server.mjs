91f0807ea30bd7191e420b0e44c0e0ed6e401fff
Markus Felten <Markus.Felten@gmx.de>
2019-04-15 21:21:50 +0200

    wip

--- src/server.mjs
+++ src/server.mjs
@@ -3,9 +3,9 @@ import { createServer as httpsCreateServer } from "https";
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import Router from "koa-better-router";
+import {createHmac} from 'crypto';
 
-export async function createServer(config, sd, requestQueue)
-{
+export async function createServer(config, sd, requestQueue) {
   const app = new Koa();
 
   app.use(bodyParser());
@@ -16,26 +16,33 @@ export async function createServer(config, sd, requestQueue)
   server.on("error", err => console.log(err));
   const router = Router();
 
-  router.addRoute(
-    "GET",
-    "/state",
-    async (ctx, next) => {
-      ctx.body = {
-        version: config.version
-      };
-      return next();
-    }
-  );
+  router.addRoute("GET", "/state", async (ctx, next) => {
+    ctx.body = {
+      version: config.version
+    };
+    return next();
+  });
 
-  router.addRoute(
-    "GET",
-    "/webhook",
-    async (ctx, next) => {
-      //obj = JSON.parse(data.toString())
-      ctx.body = {ok:true};
-      return next();
+  router.addRoute("POST", config.http.hook, async (ctx, next) => {
+    const [sig, event, id] = headers(ctx, [
+      "x-hub-signature",
+      "x-github-event",
+      "x-github-delivery"
+    ]);
+
+    console.log(ctx.request.body);
+
+    /*
+    if (!verify(sig, data)) {
+      ctx.throw('X-Hub-Signature does not match blob signature')
     }
-  );
+    */
+
+    //obj = JSON.parse(data.toString())
+
+    ctx.body = { ok: true };
+    return next();
+  });
 
   app.use(router.middleware());
 
@@ -45,4 +52,22 @@ export async function createServer(config, sd, requestQueue)
   });
 
   return server;
+}
+
+function headers(ctx, names) {
+  return names.map(name => {
+    const v = ctx.headers[name];
+    if (v === undefined) {
+      ctx.throw(400, `${name} required`);
+    }
+    return v;
+  });
+}
+
+function sign (data, secret) {
+  return 'sha1=' + createHmac('sha1', secret).update(data).digest('hex');
+}
+
+function verify (signature, data) {
+  return Buffer.from(signature).equals(Buffer.from(sign(data)));
 }
