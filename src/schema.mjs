6c06c50e10dfbbbf42595f1719a49bbe1ab1b256
Markus Felten <Markus.Felten@gmx.de>
2019-07-03 23:07:07 +0200

    feat: expose more repo attributes over graphql

--- src/schema.mjs
+++ src/schema.mjs
@@ -6,21 +6,43 @@ export const Schema = buildSchema(`
     content: String!
   }
 
+  type PullRequest {
+    name: String!
+    id: String
+    title: String
+    source: Branch
+    destination: Branch
+  }
+
+  type Hook {
+    id: String!
+    url: String!
+    events: [String]
+  }
+
   type Branch {
     name: String!
   }
 
   type RepositoryGroup {
     name: String!
+    id: String
     description: String
     repositories: [Repository]
+    repository(name: String): Repository
   }
 
   type Repository {
     name: String!
+    fullName: String!
     id: String
     description: String
+    defaultBranchName: String
+    owner: RepositoryGroup
     branches: [Branch]
+    branch(name: String): Branch
+    pullRequests: [PullRequest]
+    hooks: [Hook]
   }
 
   type Query {
