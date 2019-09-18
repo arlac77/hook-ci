b3bf222dee2b33e822846e5a96760db19af4d5b1
Markus Felten <Markus.Felten@gmx.de>
2019-09-18 11:39:41 +0200

    test: make use of .before .after hooks

--- tests/util.mjs
+++ tests/util.mjs
@@ -2,44 +2,112 @@ import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
 
-
 const here = dirname(fileURLToPath(import.meta.url));
 
-export function makeJob(id, data={}) {
+export const secret = "aSecret";
+export const hook = "webhook";
+
+export function makeJob(id, data = {}) {
   return {
     id,
     progress() {},
-    log(line) { console.log(line)},
+    log(line) {
+      console.log(line);
+    },
     data
   };
 }
 
-
-export function makeConfig(port)
-{
+export function makeConfig(port) {
   return {
     version: 99,
     http: {
-      port
+      port,
+      hooks : {
+        github: {
+          path: hook,
+          secret,
+          queue: "incoming"
+        }
+      }  
     },
     auth: {
       users: {
         user1: {
           password: "secret",
-          entitlements: ["ci","ci.nodes.read"]
+          entitlements: ["ci", "ci.nodes.read"]
         },
         user2: {
           password: "secret",
           entitlements: ["unknown"]
         }
-
       },
       jwt: {
         public: readFileSync(join(here, "fixtures", "demo.rsa.pub")),
-        private: readFileSync(join(here,  "fixtures", "demo.rsa")),  
+        private: readFileSync(join(here, "fixtures", "demo.rsa"))
       }
     }
+
   };
 }
 
 export const sd = { notify: () => {}, listeners: () => [] };
+
+export function makeQueue(name) {
+  const queue = {
+    name,
+    async getJobLogs(job, from = 0, to = 10) {
+      const lines = [];
+
+      while (from < to) {
+        lines.push(`line ${from}`);
+        from++;
+      }
+
+      return { lines };
+    },
+    async add(data) {
+      return { id: 77, queue: { name: "incoming", data } };
+    },
+    async getJobs() {
+      return [
+        {
+          id: "job1",
+          data: {
+            event: "push",
+            repository: { full_name: "repo1" },
+            ref: "refs/heads/template-sync-1"
+          }
+        },
+        {
+          id: "job2",
+          data: {
+            event: "push",
+            repository: { full_name: "repo2" },
+            ref: "refs/heads/template-sync-2"
+          }
+        }
+      ];
+    },
+    getActiveCount() {
+      return 0;
+    },
+    getWaitingCount() {
+      return 0;
+    },
+    getPausedCount() {
+      return 0;
+    },
+    getCompletedCount() {
+      return 0;
+    },
+    getFailedCount() {
+      return 0;
+    },
+    getDelayedCount() {
+      return 0;
+    }
+  };
+
+  return queue;
+}
