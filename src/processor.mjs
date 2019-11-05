db6b21242a528b77a1553018e3c9962c470d12f5
Markus Felten <Markus.Felten@gmx.de>
2019-11-05 20:26:48 +0100

    fix: publish queue

--- src/processor.mjs
+++ src/processor.mjs
@@ -32,7 +32,7 @@ export async function processJob(job, bus) {
     const m = body.match(/publish\s+(.*)/);
     if (m) {
       console.log("PUBLISH", m[1]);
-      bus.publish.add({ artifact: m[1], wd });
+      bus.queues.publish.add({ artifact: m[1], wd });
     } else {
       console.log("NOTIFICATION", body);
     }
