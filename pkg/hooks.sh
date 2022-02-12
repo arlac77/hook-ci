acf6ce6f2e419559918308c396f424ff7157c709
Markus Felten <Markus.Felten@gmx.de>
2022-02-12 02:15:32 +0100

    chore: sync cfg

--- pkg/hooks.sh
+++ pkg/hooks.sh
@@ -1,12 +1,26 @@
-
 post_install() {
-	systemctl reload nginx
+	systemctl daemon-reload
+	systemctl enable {{name}}
+	systemctl enable {{name}}.socket
+	systemctl start {{name}}.socket
+}
+
+pre_upgrade() {
+	systemctl stop {{name}}
 }
 
 post_upgrade() {
-	systemctl reload nginx
+	systemctl daemon-reload
+	systemctl restart {{name}}.socket
+}
+
+pre_remove() {
+	systemctl stop {{name}}.socket
+	systemctl disable {{name}}.socket
+	systemctl stop {{name}}
+	systemctl disable {{name}}
 }
 
 post_remove() {
-	systemctl reload nginx
+	systemctl daemon-reload
 }
