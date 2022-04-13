5e4c21f693aac7cd0cbfe4453329ec974cfaabc4
Markus Felten <Markus.Felten@gmx.de>
2022-04-13 16:21:31 +0200

    fix: switch to yaml

--- src/travis.mjs
+++ src/travis.mjs
@@ -1,10 +1,10 @@
-import yaml from "js-yaml";
+import yaml from "yaml";
 
 export async function travisAnalyse(branch, job, config, wd) {
   const steps = [];
 
   for await (const entry of branch.entries(".travis.yml")) {
-    const yml = yaml.safeLoad(await entry.string);
+    const yml = yaml.parse(await entry.string);
     console.log(yml);
   }
   return steps;
